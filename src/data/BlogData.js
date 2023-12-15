c525fa9832a00974b0bd7e42e393f05a57b452e5
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-12-15 10:43:45 +0530

    add all

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,14 +1,14 @@
 // blog data
 
 export const Blogs = [
-    // {
-    //     id:1,
-    //     name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
-    //     tags:["react","gsap","styled-components"],
-    //     date:"13 May, 2021",
-    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-    //     link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
-    // },
+    {
+        id:1,
+        name:"Video Transcoding Service using AWS Lambda and S3",
+        tags:["aws","lambda","s3"],
+        date:"15 Dec, 2023",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"/"
+    },
     // {
     //     id:2,
     //     name:"How to create React Hamburger Menu using Styled-Components",
